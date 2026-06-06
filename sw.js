246dd26216a9f6731440b54ce0235edec015bb99
khamisNEKHAL <khamis.nekhal.22@neoma-bs.com>
2026-06-06 12:33:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
-self.addEventListener('fetch', e => {});
+const CACHE = 'coach-khamis-v1';
+const BASE = '/coach-khamis';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      BASE + '/',
+      BASE + '/index.html',
+      BASE + '/manifest.json',
+      BASE + '/icons/icon-192.png',
+      BASE + '/icons/icon-512.png',
+    ]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
